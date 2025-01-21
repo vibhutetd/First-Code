a662fb5a0a53af40856544fd74bc97a322e0c527
Tanuja Dilip Vibhute <vibhutetanuja1@gmail.com>
2025-01-21 19:44:41 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,17 @@
+#Addition of two numbers
+#take two numbers from user
+first_num=int(input("Enter first number:"))
+second_num=int(input("Enter second number:"))
+#declare third variable two store result
+sum=0
+sum=first_num+second_num
+print(sum)
+
+#Add the digit in two digit number
+
+number=(input("Enter two digit number:"))
+first_digit=number[0]
+second_digit=number[1]
+print(int(first_digit)+int(second_digit))
+
+
